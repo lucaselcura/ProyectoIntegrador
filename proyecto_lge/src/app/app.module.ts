a457ba27eac0365f08735b8fefa01b63ef861c58
lucaselcura <lucaselcura@gmail.com>
2022-04-19 17:19:23 -0300

    Agragados componentes: header, logos, social y banner

--- proyecto_lge/src/app/app.module.ts
+++ proyecto_lge/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoapComponent } from './component/logoap/logoap.component';
+import { SocialComponent } from './component/social/social.component';
+import { BannerComponent } from './component/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
